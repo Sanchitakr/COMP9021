8f0365a27836e64a8d3e66bdd25d470bf4c9c221
Sanchitakr <32629833+Sanchitakr@users.noreply.github.com>
2019-09-03 14:37:47 +1000

    Add files via upload
    roman to Arabic. not perfect

---
+++ roman_arabic.py
@@ -0,0 +1,12 @@
+# Written by *** for COMP9021
+
+
+def please_convert():
+    input('How can I help you? ')
+    print("I'll think about it...")
+    # EDIT AND COMPLETE THE CODE ABOVE
+
+
+# DEFINE OTHER FUNCTIONS
+
+please_convert()
